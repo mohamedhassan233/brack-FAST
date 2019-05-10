f12033a6b7910dc2d9ddc349c6bd20cd6be14e1f
Mohamed Hassan <47718448+mohamedhassan233@users.noreply.github.com>
2019-05-10 16:58:13 +0300

    Add files via upload
    simple python script for keyboard writing

---
+++ brack-FAST.py
@@ -0,0 +1,41 @@
+
+
+
+import keyboard
+
+keyboard.add_abbreviation('"', '«»')
+keyboard.add_abbreviation('tm', '™')
+keyboard.add_abbreviation('[', '[]')
+keyboard.add_abbreviation(']', '[]')
+keyboard.add_abbreviation('ظظ', '؟')
+keyboard.add_abbreviation('@@', 'mmmhas19@gmail.com')
+keyboard.add_abbreviation('{', '{}')
+keyboard.add_abbreviation('وو', '،')
+
+
+
+while True :
+        try :
+
+            if keyboard.is_pressed('shift+9') :
+                keyboard.write(")")
+
+            elif keyboard.is_pressed('shift+0') :
+                keyboard.write(")")
+
+
+            else:
+             pass
+
+
+        except :
+            break
+
+
+
+
+
+
+
+
+
